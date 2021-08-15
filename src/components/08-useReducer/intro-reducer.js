e9a960e76ff25a1561884d3d5852fb27bb952dba
Richard n.n <richard.nunez@readinessit.com>
2021-08-14 20:04:14 -0400

    Intro Reducers

---
+++ src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,42 @@
+
+const initialState = [
+    {
+        id: 1,
+        todo: 'Comprar pan',
+        done : false
+    },
+    {
+        id: 2,
+        todo: 'Comprar leche',
+        done : false
+    }
+];
+
+const todoReducer = ( state = initialState, action ) => {
+    if ( action?.type === 'add' ){
+        return  [ ...state, action.payload ];
+    }
+
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 3,
+    todo: 'Comprar huevos',
+    done : false
+}
+
+const addTodoAction = {
+    type: 'add',
+    payload: newTodo
+}
+
+todos = todoReducer( todos, addTodoAction );
+
+
+console.log(todos);
+
+
+
