b64a077f6bc2bfd2e5f6343cece2cadf4866368a
Richard n.n <richard.nunez@readinessit.com>
2021-08-14 21:18:28 -0400

    Create HTML and use Reducer Hook
    add action created

---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,8 @@
+export const todoReducer = ( state = [], { type, payload } ) => {
+    switch ( type ) {
+        case 'add':
+            return [ ...state, payload ];
+        default:
+            return state;
+    };
+}
