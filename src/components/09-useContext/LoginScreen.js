800c21cd14f8177c84a272900882a7388ad1b47a
Richard n.n <richard.nunez@readinessit.com>
2021-08-27 00:02:29 -0400

    Creando y borrando datos con el useState compartido a travez de la app

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{ useContext } from 'react'
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+    const { setUser } = useContext(UserContext);
+    const userObj = {
+        id: 1,
+        name: 'richter'
+    } 
+
     return (
         <div>
             <h1>LoginScreen</h1>
             <hr/>
+            <button
+                className='btn btn-primary'
+                onClick={ ()=> setUser(userObj) }
+            >
+                Crear User
+            </button>
         </div>
     )
 }
