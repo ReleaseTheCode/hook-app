7e2e312d2652e4a581df34f4fed772c447f89d25
Richard n.n <richard.nunez@readinessit.com>
2021-09-01 00:43:12 -0400

    useCounter Test

--- src/test/components/hooks/useCounter.test.js
+++ src/test/components/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from '../../../hooks/useCounter'
 
 describe('Pruebas en useCounter', () => {
@@ -10,5 +10,28 @@ describe('Pruebas en useCounter', () => {
         expect ( typeof result.current.decrement).toBe('function')
         expect ( typeof result.current.reset).toBe('function')
     })
+
+    test('should increment counter by 1', () => {
+        const { result } = renderHook( ()=> useCounter(100) );
+        const { increment, counter } = result.current;
+        act( () => increment() );
+        expect ( counter).toBe(100);
+    });
     
+    test('should decrement counter by 1', () => {
+        const { result } = renderHook( ()=> useCounter(100) );
+        const { decrement} = result.current;
+        act( () => decrement() );
+        const { counter } = result.current;
+        expect ( counter).toBe(99);
+    });
+    test('should reset counter after decrement', () => {
+        const { result } = renderHook( ()=> useCounter(100) );
+        const { reset, decrement, counter } = result.current;
+        act( () => {
+            decrement();
+            reset();
+        });
+        expect ( counter ).toBe(100);
+    });
 })
