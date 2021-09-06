1b4e058d6a694535b2505e2260f60db0aa286cac
Richard n.n <richard.nunez@readinessit.com>
2021-09-05 22:19:24 -0300

    Init testing on reducers
    test default/add response
    add fixtures for demo data

---
+++ src/test/components/08-useReducer/todoReducer.test.js
@@ -0,0 +1,23 @@
+import { todoReducer } from "../../../components/08-useReducer/todoReducer"
+import { demoTodos } from "../../fixrures/demoTodos"
+
+describe('Test on ToDo Reducer', () => {
+
+    test('should return default values', () => {
+        const state = todoReducer( demoTodos, {} );
+        expect( demoTodos ).toEqual(state);
+    });
+
+    test('should return the add result', () => {
+        const newTodo = {
+            id: 3,
+            todo: 'Comprar Cafe',
+            done : true
+        }
+        
+        const state = todoReducer( demoTodos, { type: 'add', newTodo } );
+        expect( state.length ).toBe( 3 );
+        expect( state ).toEqual( [ ...demoTodos, newTodo ] );
+    });
+    
+});
---
+++ src/test/fixtures/demoTodos.js
@@ -0,0 +1,12 @@
+export const demoTodos = [
+    {
+        id: 1,
+        todo: 'Comprar pan',
+        done : false
+    },
+    {
+        id: 2,
+        todo: 'Comprar leche',
+        done : false
+    }
+];
